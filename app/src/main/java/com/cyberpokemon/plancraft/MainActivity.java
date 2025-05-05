1c2ee3620e1ee31b473550117e6f3f585d22ba03
CyberPokemon <imoncoding@gmail.com>
2025-05-05 21:22:07 +0530

    splash screen code ready

--- app/src/main/java/com/cyberpokemon/plancraft/MainActivity.java
+++ app/src/main/java/com/cyberpokemon/plancraft/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cyberpokemon.plancraft;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    public static final String PREF_NAME= "PlanCraftPreferences";
+    public static final String KEY_IS_REGISTERED= "is_registered";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences=getSharedPreferences(PREF_NAME,MODE_PRIVATE);
+        boolean isRegistered=sharedPreferences.getBoolean(KEY_IS_REGISTERED,false);
 
+        if(isRegistered)
+        {
+            startActivity(new Intent(MainActivity.this,TodoActivity.class));
+        }
+        else
+        {
+            startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+        }
+        finish();
     }
 }
